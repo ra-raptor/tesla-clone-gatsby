8568e0b3702d898983218ee65f7a589e0b826739
Vikas Kumar Saw <vikasvks0112@gmail.com>
2022-01-23 21:50:43 +0530

    basic sign up page

---
+++ src/pages/signup.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+export default function signup() {
+  return (
+    <Layout>
+      <Seo title="Signup" />
+      SIGNUP
+    </Layout>
+  )
+}
